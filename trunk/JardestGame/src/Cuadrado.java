baaec1e57c35d9dba65c45f68cebb6d096b15471
jorge.gh91@gmail.com <jorge.gh91@gmail.com@864d85e6-197e-2899-4623-48dd8c639d58>
2011-07-07 23:45:38 +0000

    cuadrado velocidad

--- trunk/JardestGame/src/Cuadrado.java
+++ trunk/JardestGame/src/Cuadrado.java
@@ -1,6 +1,7 @@
 
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FocusTraversalPolicy;
 import java.awt.Graphics;
 import java.awt.Rectangle;
@@ -22,7 +23,7 @@ public class Cuadrado extends JComponent implements KeyListener{ //jcomponent pa
 	private int x, y;
 	private float vx, vy; //para fisica()
 	private boolean presionado, arriba, abajo, derecha, izquierda; //aux, para que al dejar pulsada la tecla no se inicie el hilo muchas veces
-	private Thread hiloMovimiento; //hilo solo para mover cuadrado y repintarlo
+	private Velocidad velocidad;
 	
 	Cuadrado(){
 		this.altura = 20;
@@ -30,40 +31,26 @@ public class Cuadrado extends JComponent implements KeyListener{ //jcomponent pa
 		this.x=0;
 		this.y=200;
 		this.presionado = false;
+		this.velocidad = new Velocidad(0,0); //velocidad por defecto?
 		setFocusable(true);
 		requestFocusInWindow();
 		addKeyListener(this);
-		hiloMovimiento=new Thread(){
-			public void run() {
-				//long tiempoViejo = System.nanoTime();
-				while(presionado){
-		            //long tiempoNuevo = System.nanoTime();
-		            //float dt = (tiempoNuevo - tiempoViejo) / 1000000000f;
-		            //tiempoViejo = tiempoNuevo;
-					//fisica(dt);
-					desplazar();
-					try {
-						repintar();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
 	}
 	
-	Cuadrado(int altura){
+	Cuadrado(int altura, int i, int j){
 		this.altura = altura;
 		this.mitadAltura = altura/2;
 		this.x=0;
 		this.y=200;
+		this.presionado = false;
+		this.velocidad = new Velocidad(i, j);
 		setFocusable(true);
 		requestFocusInWindow();
 		addKeyListener(this);
 	}
 	
 	public int getX() {
-		return Math.round(x);
+		return x;
 	}
 
 	public void setX(int x) {
@@ -71,7 +58,7 @@ public class Cuadrado extends JComponent implements KeyListener{ //jcomponent pa
 	}
 
 	public int getY() {
-		return Math.round(y);
+		return y;
 	}
 
 	public void setY(int y) {
@@ -93,74 +80,32 @@ public class Cuadrado extends JComponent implements KeyListener{ //jcomponent pa
 	public void setMitadAltura(int mitadAltura) {
 		this.mitadAltura = mitadAltura;
 	}
-
-	//desplazar en eje x e y de 6 en 6
-	public void desplazar(){
-		if(arriba)
-			y-=6;
-		if(abajo)
-			y+=6;
-		if(derecha)
-			x+=6;
-		if(izquierda)
-			x-=6;
-	}
 	
-	//copiado
-	 private void fisica(float dt) {
-		 vx = 0;
-	     vy = 0;
-	     if (arriba)
-	    	 vy = -300;
-	     if (abajo)
-	            vy = 300;
-	        if (izquierda)
-	            vx = -300;
-	        if (derecha)
-	            vx = 300;
-	        //x = clamp(x + vx * dt, 0, 500 - altura);
-	        //y = clamp(y + vy * dt, 0, 500 - altura);
-	}
-	 
-	 //copiado
-	 private float clamp(float valor, float min, float max) {
-	        if (valor > max)
-	            return max;
-	        if (valor < min)
-	            return min;
-	        return valor;
-	    }
-	
-	private void repintar() throws Exception{ //llamarlo desde el hilo
-		 SwingUtilities.invokeAndWait(new Runnable() {
-             public void run() {
-                 repaint();
-             }
-         });
+	public void mover(double dt){
+		Dimension dim = velocidad.mover(dt);
+		this.x = dim.width;
+		this.y = dim.height;
 	}
 	
 	public void paint(Graphics g) {
 		g.setColor(Color.green);
-		g.fillRect(Math.round(x), Math.round(y), mitadAltura, mitadAltura);
+		g.fillRect(x, y, mitadAltura, mitadAltura);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		actualizar(e.getKeyCode(),true);
-		if(!this.presionado){ //si es la primera vez que pulso la tecla (no multiples evento de dejarla pulsada)
+		//si lo metemos dentro de !presionado, si queda alguna pulsada (true), al hacer released no llamara a actualizar y no cambiara la velocidad
+		actualizar(e.getKeyCode(),5);
+		/*if(!this.presionado){ //si es la primera vez que pulso la tecla (no multiples evento de dejarla pulsada)
 			this.presionado=true;
-			Thread hilo = new Thread(hiloMovimiento); //apaño para volver a lanzar el hilo despues de hacer released
-			hilo.start();
-		}
+		}*/
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		actualizar(e.getKeyCode(),false);
-		this.presionado = arriba || abajo || derecha || izquierda;
-		if(!presionado)
-			hiloMovimiento.interrupt();
+		actualizar(e.getKeyCode(), 0);
+		//this.presionado = arriba || abajo || derecha || izquierda;
 	}
 
 	@Override
@@ -169,23 +114,27 @@ public class Cuadrado extends JComponent implements KeyListener{ //jcomponent pa
 	}
 	
 	//actualizar direccion de movimiento
-	private void actualizar(int keyCode, boolean presionado){
+	private void actualizar(int keyCode, double valor){
 		 switch (keyCode) {
          case KeyEvent.VK_UP:
-             arriba = presionado;
-             break;
+             //arriba = presionado;
+             velocidad.setJ(-valor);
+        	 break;
 
          case KeyEvent.VK_DOWN:
-             abajo = presionado;
-             break;
+             //abajo = presionado;
+             velocidad.setJ(valor);
+        	 break;
 
          case KeyEvent.VK_LEFT:
-             izquierda = presionado;
-             break;
+             //izquierda = presionado;
+             velocidad.setI(-valor);
+        	 break;
 
          case KeyEvent.VK_RIGHT:
-             derecha = presionado;
-             break;
+             //derecha = presionado;
+             velocidad.setI(valor);
+        	 break;
 		 }
 	}
 }
