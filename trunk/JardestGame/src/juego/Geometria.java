1a56b10e974aa58febf6a1459bdf3b653b71b56a
jorge.gh91@gmail.com <jorge.gh91@gmail.com@864d85e6-197e-2899-4623-48dd8c639d58>
2011-07-13 15:45:50 +0000

    matriz filas por columnas

--- trunk/JardestGame/src/juego/Geometria.java
+++ trunk/JardestGame/src/juego/Geometria.java
@@ -8,47 +8,47 @@ public class Geometria {
 	
 	Geometria(Dimension sizePanel, int sizeLosa){
 		this.sizeLosa = sizeLosa;
-		this.matriz = new char[sizePanel.width/sizeLosa][sizePanel.height/sizeLosa];
+		this.matriz = new char[sizePanel.height/sizeLosa][sizePanel.width/sizeLosa];
 		
 		crearNivel();
 	}
 	
 	private void crearNivel() {
-		for(int i=0; i<matriz.length; i++){
-			for(int j=0; j<matriz[0].length; j++){
-				if(i==0 || i==1 || i==2)
+		for(int j=0; j<matriz[0].length; j++){
+			for(int i=0; i<matriz.length; i++){
+				if(j==0 || j==1 || j==2)
 					matriz[i][j] = '2'; //zona segura inicial
-				if(i==3){
-					if(j==9)
+				if(j==3){
+					if(i==9)
 						matriz[i][j] = '1';
 					else
 						matriz[i][j] = '0';
 				}
-				if(i==4){
-					if(j==0)
+				if(j==4){
+					if(i==0)
 						matriz[i][j] = '0';
 					else
 						matriz[i][j] = '1';
 				}
-				if(i==5 || i==6 || i==7 || i==8 || i==9 || i==10){
-					if(j==0 || j==9)
+				if(j==5 || j==6 || j==7 || j==8 || j==9 || j==10){
+					if(i==0 || i==9)
 						matriz[i][j] = '0';
 					else
 						matriz[i][j] = '1';
 				}
-				if(i==11){
-					if(j==9)
+				if(j==11){
+					if(i==9)
 						matriz[i][j] = '0';
 					else
 						matriz[i][j] = '1';
 				}
-				if(i==12){
-					if(j==0)
+				if(j==12){
+					if(i==0)
 						matriz[i][j] = '1';
 					else
 						matriz[i][j] = '0';
 				}
-				if(i==13 || i==14 || i==15){
+				if(j==13 || j==14 || j==15){
 					matriz[i][j] = '3'; //zona de meta
 				}
 			}
@@ -60,11 +60,17 @@ public class Geometria {
 		i = (int)punto1.getX()/sizeLosa;
 		j = (int)punto1.getY()/sizeLosa;
 		
+		if(i >= matriz.length+1 || j >= matriz[0].length+1)
+			return false;
+		
 		if(matriz[i][j] == 0)
 			return false;
 		
 		i = (int)punto2.getX()/sizeLosa;
 		j = (int)punto2.getY()/sizeLosa;
+		
+		if(i >= matriz.length+1 || j >= matriz[0].length+1)
+			return false;
 		
 		if(matriz[i][j] == 0)
 			return false;
