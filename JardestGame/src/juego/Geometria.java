47c9f7b0aa54520109c41bf0918f004f5b9a8cd0
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2011-07-13 16:47:14 +0000

    colisiones sin rebote

--- JardestGame/src/juego/Geometria.java
+++ JardestGame/src/juego/Geometria.java
@@ -17,39 +17,39 @@ public class Geometria {
 		for(int j=0; j<matriz[0].length; j++){
 			for(int i=0; i<matriz.length; i++){
 				if(j==0 || j==1 || j==2)
-					matriz[i][j] = '2'; //zona segura inicial
+					matriz[i][j] = 2; //zona segura inicial
 				if(j==3){
 					if(i==9)
-						matriz[i][j] = '1';
+						matriz[i][j] = 1;
 					else
-						matriz[i][j] = '0';
+						matriz[i][j] = 0;
 				}
 				if(j==4){
 					if(i==0)
-						matriz[i][j] = '0';
+						matriz[i][j] = 0;
 					else
-						matriz[i][j] = '1';
+						matriz[i][j] = 1;
 				}
 				if(j==5 || j==6 || j==7 || j==8 || j==9 || j==10){
 					if(i==0 || i==9)
-						matriz[i][j] = '0';
+						matriz[i][j] = 0;
 					else
-						matriz[i][j] = '1';
+						matriz[i][j] = 1;
 				}
 				if(j==11){
 					if(i==9)
-						matriz[i][j] = '0';
+						matriz[i][j] = 0;
 					else
-						matriz[i][j] = '1';
+						matriz[i][j] = 1;
 				}
 				if(j==12){
 					if(i==0)
-						matriz[i][j] = '1';
+						matriz[i][j] = 1;
 					else
-						matriz[i][j] = '0';
+						matriz[i][j] = 0;
 				}
 				if(j==13 || j==14 || j==15){
-					matriz[i][j] = '3'; //zona de meta
+					matriz[i][j] = 3; //zona de meta
 				}
 			}
 		}
@@ -57,8 +57,8 @@ public class Geometria {
 	}
 
 	public boolean esValido(Punto punto1, Punto punto2){
-		i = (int)punto1.getX()/sizeLosa;
-		j = (int)punto1.getY()/sizeLosa;
+		i = (int)punto1.getY()/sizeLosa; //filas
+		j = (int)punto1.getX()/sizeLosa; //columnas
 		
 		//si se supera el maximo tamaño de la matriz (colision pared derecha o inferior)
 		if(i > matriz.length || j > matriz[0].length){
@@ -78,8 +78,8 @@ public class Geometria {
 		if(matriz[i][j] == 0)
 			return false;
 		
-		i = (int)punto2.getX()/sizeLosa;
-		j = (int)punto2.getY()/sizeLosa;
+		i = (int)punto2.getY()/sizeLosa;
+		j = (int)punto2.getX()/sizeLosa;
 		
 		if(i > matriz.length || j >= matriz[0].length){
 			i = matriz.length;
@@ -99,8 +99,12 @@ public class Geometria {
 		return true;
 	}
 	
-	public int getPared(){
+	public int getParedIzq(){
 		return i*sizeLosa; //devuelve arista izquierda de la losa
+	}
+	
+	public int getParedSup(){
+		return j*sizeLosa; //devuelve arista izquierda de la losa
 	}
 	
 	public char[][] getMatriz(){
