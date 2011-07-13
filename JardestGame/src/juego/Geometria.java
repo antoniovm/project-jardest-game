527e4c0974e58452e63d0d8bd64aa6f1b29a05dc
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2011-07-13 10:19:23 +0000

    nivel 1 mapeado

--- JardestGame/src/juego/Geometria.java
+++ JardestGame/src/juego/Geometria.java
@@ -8,16 +8,54 @@ public class Geometria {
 	
 	Geometria(Dimension sizePanel, int sizeLosa){
 		this.sizeLosa = sizeLosa;
-		this.matriz = new char[sizePanel.height/sizeLosa][sizePanel.width/sizeLosa];
+		this.matriz = new char[sizePanel.width/sizeLosa][sizePanel.height/sizeLosa];
 		
-		/*for(int j=0; j<matriz.length; j++){
-			for(int i=0; i<matriz[0].length; i++){
-				if(j==0)
-					matriz[i][j] = 0;
-			}
-		}*/
+		crearNivel();
 	}
 	
+	private void crearNivel() {
+		for(int i=0; i<matriz.length; i++){
+			for(int j=0; j<matriz[0].length; j++){
+				if(i==0 || i==1 || i==2)
+					matriz[i][j] = '2'; //zona segura inicial
+				if(i==3){
+					if(j==9)
+						matriz[i][j] = '1';
+					else
+						matriz[i][j] = '0';
+				}
+				if(i==4){
+					if(j==0)
+						matriz[i][j] = '0';
+					else
+						matriz[i][j] = '1';
+				}
+				if(i==5 || i==6 || i==7 || i==8 || i==9 || i==10){
+					if(j==0 || j==9)
+						matriz[i][j] = '0';
+					else
+						matriz[i][j] = '1';
+				}
+				if(i==11){
+					if(j==9)
+						matriz[i][j] = '0';
+					else
+						matriz[i][j] = '1';
+				}
+				if(i==12){
+					if(j==0)
+						matriz[i][j] = '1';
+					else
+						matriz[i][j] = '0';
+				}
+				if(i==13 || i==14 || i==15){
+					matriz[i][j] = '3'; //zona de meta
+				}
+			}
+		}
+		
+	}
+
 	public boolean getValido(Punto punto1, Punto punto2){
 		i = (int)punto1.getX()/sizeLosa;
 		j = (int)punto1.getY()/sizeLosa;
